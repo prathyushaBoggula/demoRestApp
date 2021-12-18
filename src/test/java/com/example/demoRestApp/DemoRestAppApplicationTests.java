219c1d2f674365d69d90fe0e432212c30c04cde9
fa9ol60 <shilpareddy.sangala@fisev.com>
2021-12-17 22:46:44 -0500

    go

---
+++ src/test/java/com/example/demoRestApp/DemoRestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoRestApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoRestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
